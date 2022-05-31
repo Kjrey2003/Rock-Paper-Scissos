bae43e280a5e8257c97559a282ea1e548996fe62
Kjrey2003 <kajanirey@gmail.com>
2022-05-31 16:04:02 -0400

    finally fucking finished, added a class to loop 5 times, initialized the score variables outside of oneRound so they wont clear and just fixed up a ton of bugs

--- Rockpaper.js
+++ Rockpaper.js
@@ -1,55 +1,114 @@
+
+let playerScore = 0;
+let computerScore = 0; 
+
+
 function computerPlay(){
     let options = ["rock", "paper", "scissors"];
     let rand = options[(Math.floor(Math.random() * options.length))];
     return rand;
 }
 
-function select(){
-    let playerSelection;
-          playerSelection = console.log("Enter Rock, Paper, or scissors");
-    }
-    
 
+
+         
 function oneRound(computerSelection, playerSelection){
     computerSelection = computerPlay();
-    let   result = ''
-    let playerScore = 0;
-    let computerScore = 0;
+    playerSelection =  prompt("Enter rock, paper, or scissors");
+    result = '';
+  
+   
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    
+        if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock'))
     {
-        playerScore +=1;
-       result = console.log("You won this round because " + playerSelection + " beats " + computerSelection);
+       playerScore +=1;
+        result = alert("You won this round becase " + playerSelection + " beats " + computerSelection + " your score is " + playerScore)
+       
+       
+
+           
     }
-    else if (computerScore+=1){
-       computerScore+=1
-        result = console.log("You lost this round because " + computerSelection + " beats " + playerSelection);
+    else if (
+        (computerSelection == 'rock' && playerSelection == 'scissors') ||
+        (computerSelection == 'scissors' && playerSelection == 'paper') ||
+        (computerSelection == 'paper' && playerSelection == 'rock'))
+        {
+            computerScore+=1
+            result = alert("You lost this round because " + computerSelection + " beats " + playerSelection + " the computer's score  " + computerScore + 
+            " your score " + playerScore);
+        
+        }
+
+   if(playerSelection === computerSelection){
+            result =  alert("It is a tie because " + playerSelection + " is the same as " + computerSelection + "  your score is " + 
+            playerScore );
+             }
+
+             if(computerScore == 5){
+                result = console.log("Computer wins")
+            }
+        
+        
+            if(playerScore == 5){
+                result =  console.log("Player wins");
+             }
+               
+        if(computerScore == 5){
+            result = alert("Computer wins")
+        }
+        
+        
+        if(playerScore == 5){
+            result =  alert("Player wins");
+         }
+           
+         return result;
+        
+        }
+        
+        function countingScore(){
+            let scoreTrackUser = playerScore;
+            let scoreTrackComputer = computerScore
+        }
+        
+        
+        function game(){
+ 
+    for(let i = 0; i < 5; i++){
+    oneRound();
+    
+            }
+
+            
+    if(playerScore > computerScore){
+        alert("You won and your score " + playerScore  + " points" )
+    }
+    else if (computerScore > playerScore){
+        alert("You lost and your score was " + playerScore + " points" )
     }
 
-    else if(playerScore == 5){
-       result =  console.log("Player wins");
-    }
-    else if(computerScore == 5){
-        result = console.log("Computer wins")
-    }
-     else if(playerScore === computerScore) {
+    else{
+        alert("The game is a tie your score is " + playerScore + " and the computer's is " + computerScore)
+}
+ 
+    
+    return result;
 
-        console.log("The game is a tie")
 
-    }
+   
+   
+   // result = alert("You won this round because " + playerSelection + " beats " + computerSelection + " your score " + playerScore + 
+    // "  computer's score " + computerScore);
 
-     else if(playerSelection === computerSelection){
-         console.log("It is a tie because " + playerSelection + " is the same as " + computerSelection)
-    }
+
+    
+    
+    
+    
+    //return result;
 
 
 }
-
-function fiveTime(){
-    for(i=0; i <=6; i++){
-        oneRound();
-    }
-}
-
